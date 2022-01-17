fdd9040711941b647bb9bdb0315eb8610275f2de
Sourabh Shrivastava <simplysourabh_123@yahoo.com>
2022-01-17 13:59:36 +0530

    Implement delete task functionality and mongodb connectoin

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 
 const port = 8000;
+
+const db = require('./config/mongoose');
+const TodoList = require('./models/todolist');
+
 const app = express();
 
 // Setting view engine and view path
@@ -12,18 +16,56 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded());
 
 // Including static pages - contained in folder 'static'
-app.use(express.static('static'));
+var publicDir = path.join(__dirname,'/static'); 
+app.use(express.static(publicDir)); 
 
 // Controller - Home page
 app.get('/', (req, res) => {
-    res.render('home');
+    TodoList.find({}, (err, todolist) => {
+        if (err) {
+            console.log('Error in fetching the contacts');
+            return;
+        }
+        res.render('home', {
+            todo_list: todolist
+        });
+    })
 });
 
 // Controller - Create task
-
+app.post('/create-task', (req, res) => {
+    // Appending the new task to our task list
+    // contactList.push(req.body);
+    TodoList.create({ 
+        description: req.body.description, 
+        category: req.body.category,
+        duedate: req.body.duedate
+    }, function (err, newTask) {
+            if (err) {
+                console.log("Error in creating task");
+                return;
+            }
+            console.log("********", newTask);
+            res.redirect('/');
+        }
+    );
+});
 
 // Controller - Delete task
+app.get('/delete-task', (req, res) => {
+    // get the id from query
+    let id = req.query.id;
 
+    // find the contact in database entry
+    TodoList.findByIdAndDelete(id, (err) => {
+        if (err) {
+            console.log("Error in deleting contact from database");
+            return;
+        }
+        return res.redirect('/');
+    });
+    
+});
 
 // Bind and listen the connections on the specified port
 app.listen(port, (err) => {
